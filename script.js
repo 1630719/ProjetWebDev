be56eb598f500157c50e8be4b02b5526e32ffa34
XavierA <1630719@cegepoutaouais.qc.ca>
2023-11-04 22:47:06 -0400

    Révision avant d'envoyer!

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
+// Champs
+let $NomProprio = $("#NomProprio")
+let $courriel = $("#Courriel")
+let $NumTel = $("#NumTel")
+let $NomAnimal = $("#NomAnimal")
+let $AgeAnimal = $("#AgeAnimal")
+let $PoidsAnimal = $("#PoidsAnimal")
+let $TypeAnimal = $("#TypeAnimal")
+let $DateRdv = $("#DatePicker")
 
-let $form = $("#Form")
-let $ListeRDV = $("#ListeRDV")
-let $compteur = 0
-
+// Labels erreur
+let $LabelCourriel = $("#ErreurCourriel")
+let $LabelProprio = $("#ErreurNomProprio")
+let $LabelNumTel = $("#ErreurNumTel")
+let $LabelNomAnimal = $("#ErreurNomAnimal")
 
 // Boutons
 const $btn_annuler = $("#BoutonAnnuler")
 
-// Champs
-let $courriel = $("#Courriel")
-let $NomProprio = $("#NomProprio")
-let $NumTel = $("#NumTel")
+//Autres selecteurs
+let $form = $("#Form")
+let $ListeRDV = $("#ListeRDV")
+let $compteur = 0
+let $ChaineBold = ("Informations du rendez-vous : ")
 
-// Labels erreur
-let $LabelCourriel = $("#ErreurCourriel")
-let $LabelPrprio = $("#ErreurNomProprio")
-let $LabelNumTel = $("#ErreurNumTel")
+//Code JS pour avoir la date du jour comme date minimale (valider que le rdv n'est pas à une date passée)
+let date = new Date().toISOString().split('T')[0]
+$DateRdv[0].setAttribute('min', date);
 
-//Pour repartir de 0
-$btn_annuler.on('click', function ()
-{
-    window.location.reload()
-})
+
 
 // SECTION FONCTIONS VALIDATION/LABEL ERREUR
+
+//Validation nom du propriétaire
+$NomProprio.on('blur', function ()
+{
+    if (!$NomProprio.val().match("^[A-Z]{1}[a-zA-Z]{2,10}\\s[a-zA-Z\\-\\/]{5,15}$"))
+    {
+        $LabelProprio.text("Le nom doit être entre 3-25 caractères et débuter par une majuscule.")  // Afficher erreur
+    }
+    else
+    {
+        $LabelProprio.text("")  // Vider/Garder vide si valide
+    }
+})
 
 //Validation du courriel
 $courriel.on('blur', function ()
@@ -39,39 +58,74 @@ $courriel.on('blur', function ()
 
 })
 
-//Validation nom du propriétaire
-$NomProprio.on('blur', function ()
-{
-    if (!$NomProprio.val().match("^[A-Z]{1}[a-zA-Z]{2,10}\\s[a-zA-Z\\-\\/]{5,15}$"))
-    {
-        $LabelPrprio.text("Le nom doit être entre 5-25 caractères et débuter par une majuscule.")  // Afficher erreur
-    }
-    else
-    {
-        $LabelPrprio.text("")  // Vider/Garder vide si valide
-    }
-})
-
 //Validation numéro de telephone
 $NumTel.on('blur', function ()
 {
     if (!$NumTel.val().match("^[0-9]{3}-[0-9]{3}-[0-9]{4}$"))
     {
-        $LabelNumTel.text("Le numéro doit suivre l'exemple suivant: 819-555-5555 (tiret entre les chiffres)")
+        $LabelNumTel.text("Le numéro doit suivre l'exemple suivant: 819-555-5555 (tiret entre les chiffres)") // Afficher l'erreur
     }
     else
     {
-        $LabelNumTel.text("")
+        $LabelNumTel.text("") // Vider/garder vide si bon
+    }
+})
+
+//Validation du nom de l'animal
+$NomAnimal.on('blur', function ()
+{
+    if (!$NomAnimal.val().match("^[A-Z]{1}[a-zA-Z]{2,10}\\s[a-zA-Z\\-\\/]{5,15}$"))
+    {
+        $LabelNomAnimal.text("Le nom doit débuter par une majuscule et entre 3-25 caractères")
+    }
+    else
+    {
+        $LabelNomAnimal.text("")
     }
 })
 
 $form.on("submit", function ()
 {
-    $compteur++  // Incrémenter de 1
+    //Récupérer le sexe de l'animal
+    let $SexeAnimal = $("input[type='radio'][name=SexeAnimal]:checked", '#Form')
 
-    $ListeRDV.append("Courriel : "+ "<span>" + $courriel.val() + "</span>" + "<br>" +
-                     "Nom propriétaire : " + "<span>" + $NomProprio.val() + "</span>" + "<br>")
+    $compteur++  // Incrémenter de 1
+    if ($compteur === 1)
+    {
+        $ListeRDV.append( "Courriel : "+ "<span>" + $courriel.val() + "</span>" + "<br>" +
+            "Nom propriétaire : " + "<span>" + $NomProprio.val() + "</span>" + "<br>" +
+            "Numéro de cellulaire : " + "<span>" + $NumTel.val() + "</span>" + "<br>" +
+            "Nom de l'animal : " + "<span>" + $NomAnimal.val() + "</span>" +  "<br>" +
+            "Age de l'animal : " + "<span>" + $AgeAnimal.val() + "</span>" + "<br>" +
+            "Type d'animal : " + "<span>" + $TypeAnimal.val() + "</span>" + "<br>" +
+            "Poids de l'animal : " + "<span>" + $PoidsAnimal.val() +" lbs" + "</span>" + "<br>" +
+            "Sexe de l'animal : " + "<span>" + $SexeAnimal.val() + "</span>" + "<br>" +
+            "Date du rendez-vous :" + "<span>" + $DateRdv.val() + "</span>" +
+            "<br>" + "<br>")
+    }
+
+    else if ($compteur > 1)
+    {
+        $ListeRDV.append($ChaineBold + "<br>" + "Courriel : "+ "<span>" + $courriel.val() + "</span>" + "<br>" +
+            "Nom propriétaire : " + "<span>" + $NomProprio.val() + "</span>" + "<br>" +
+            "Numéro de cellulaire : " + "<span>" + $NumTel.val() + "</span>" + "<br>" +
+            "Nom de l'animal : " + "<span>" + $NomAnimal.val() + "</span>" +  "<br>" +
+            "Age de l'animal : " + "<span>" + $AgeAnimal.val() + "</span>" + "<br>" +
+            "Type d'animal : " + "<span>" + $TypeAnimal.val() + "</span>" + "<br>" +
+            "Poids de l'animal : " + "<span>" + $PoidsAnimal.val() +" lbs" + "</span>" + "<br>" +
+            "Sexe de l'animal : " + "<span>" + $SexeAnimal.val() + "</span>" + "<br>" +
+            "Date du rendez-vous :" + "<span>" + $DateRdv.val() + "</span>" +
+            "<br>" + "<br>")
+    }
 
 })
+
+//Pour repartir de 0
+$btn_annuler.on('click', function ()
+{
+    window.location.reload()
+})
+
+
 
 
